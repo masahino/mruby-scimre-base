f8d53ef23417bf1e6c22f3b6bd8abf5321666fcb
masahino <masahino@gmail.com>
2016-01-21 13:43:51 +0900

    move from mrbmacs-curses

---
+++ mrblib/ui.rb
@@ -0,0 +1,74 @@
+# coding: utf-8
+module Mrbmacs
+  class Frame
+    include Scintilla
+    attr_accessor :view_win, :echo_win, :tk
+
+    def init_keysyms
+      @keysyms = [0,
+                  Scintilla::SCK_BACK,
+                  Scintilla::SCK_TAB,
+                  Scintilla::SCK_RETURN,
+                  Scintilla::SCK_ESCAPE,
+                  0,
+                  Scintilla::SCK_BACK,
+                  Scintilla::SCK_UP,
+                  Scintilla::SCK_DOWN,
+                  Scintilla::SCK_LEFT,
+                  Scintilla::SCK_RIGHT,
+                  0,0,
+                  Scintilla::SCK_INSERT,
+                  Scintilla::SCK_DELETE,
+                  0,
+                  Scintilla::SCK_PRIOR,
+                  Scintilla::SCK_NEXT,
+                  Scintilla::SCK_HOME,
+                  Scintilla::SCK_END]
+    end
+
+    def set_default_style
+      @view_win.sci_set_codepage(Scintilla::SC_CP_UTF8)
+      @view_win.sci_set_margin_widthn(0, @view_win.sci_text_width(Scintilla::STYLE_LINENUMBER, "_99999"))
+      @view_win.sci_set_margin_maskn(0, ~Scintilla::SC_MASK_FOLDERS)
+      @view_win.sci_set_margin_widthn(1, 1)
+      @view_win.sci_set_margin_typen(1, 0)
+      @view_win.sci_set_margin_maskn(1, Scintilla::SC_MASK_FOLDERS)
+      #@echo_win.sci_style_set_fore(Scintilla::STYLE_DEFAULT, Scintilla::COLOR_WHITE)
+      @echo_win.sci_style_set_fore(Scintilla::STYLE_DEFAULT, 0xffffff)
+      #@echo_win.sci_style_set_back(Scintilla::STYLE_DEFAULT, Scintilla::COLOR_BLACK)
+      @echo_win.sci_style_set_back(Scintilla::STYLE_DEFAULT, 0x000000)
+      @echo_win.sci_style_clear_all()
+      @echo_win.sci_auto_cset_choose_single(1)
+      @echo_win.sci_auto_cset_auto_hide(false)
+    end      
+      
+    def get_mode_str(app)
+      newline = case @view_win.sci_get_eol_mode
+      when 0
+        "dos"
+      when 1
+        "mac"
+      when 2
+        "unix"
+      else
+        ""
+      end
+      mode_text = " (#{app.current_buffer.encoding}-#{newline}):"
+      if @view_win.sci_get_modify != 0
+        mode_text += "**-"
+      else
+        mode_text += "---"
+      end
+      mode_text += sprintf("    %-20s", app.current_buffer.name)
+      x = @view_win.sci_get_column(@view_win.sci_get_current_pos)+1
+      y = @view_win.sci_line_from_position(@view_win.sci_get_current_pos)+1
+      mode_text += sprintf("%-10s", "(#{x},#{y})")
+      mode_text += "["+app.mode.name+"]"
+    end      
+
+    def read_buffername(prompt)
+      echo_gets(prompt)
+    end
+
+  end
+end
