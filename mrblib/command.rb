5d707a05464661d0b8d0b2bf99d7fc125dd885de
masahino <masahino@gmail.com>
2016-09-21 22:39:07 +0900

    change command list

--- mrblib/command.rb
+++ mrblib/command.rb
@@ -2,7 +2,8 @@ module Mrbmacs
   class Application
     def execute_extended_command()
       echo_win = @frame.echo_win
-      command_list = @command_list.values.select {|item| item.kind_of?(String)}
+#      command_list = @command_list.values.select {|item| item.kind_of?(String)}
+      command_list = (Mrbmacs::Application.instance_methods - Object.instance_methods).map {|item| item.to_s}
       command = @frame.echo_gets("M-x ") do |input_text|
         command_candidate = command_list.select {|item| item =~ /^#{input_text}/}
         [command_candidate.join(" "), input_text.length]
