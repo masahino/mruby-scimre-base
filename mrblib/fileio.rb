c13ecb15b1ad4793868cd7192f8193fa1e1a361d
masahino <masahino@gmail.com>
2017-03-25 22:41:27 +0900

    improve find-file

--- mrblib/fileio.rb
+++ mrblib/fileio.rb
@@ -89,5 +89,35 @@ module Mrbmacs
         view_win.sci_insert_text(view_win.sci_get_current_pos, text)
       end
     end
+
+    def find_file(filename = nil)
+      view_win = @frame.view_win
+
+      if filename == nil
+        dir = @current_buffer.directory
+        filename = read_file_name("find file: ", dir)
+      end
+      if filename != nil
+        if Mrbmacs::get_buffer_from_name(@buffer_list, File.basename(filename)) != nil
+          switch_to_buffer(File.basename(filename))
+        else
+          @current_buffer.pos = view_win.sci_get_current_pos
+          new_buffer = Buffer.new(filename, @buffer_list)
+          view_win.sci_add_refdocument(@current_buffer.docpointer)
+          view_win.sci_set_docpointer(nil)
+          new_buffer.docpointer = view_win.sci_get_docpointer
+#         new_buffer.docpointer = view_win.create_document
+          @buffer_list.push(new_buffer)
+          @prev_buffer = @current_buffer
+          @current_buffer = new_buffer
+          load_file(filename)
+          view_win.sci_set_lexer_language(@current_buffer.mode.name)
+          @current_buffer.mode.set_style(view_win, @theme)
+          view_win.sci_set_sel_back(true, 0xff0000)
+          @frame.set_buffer_name(@current_buffer.name)
+#        view_win.sci_refresh
+        end
+      end
+    end
   end
 end
