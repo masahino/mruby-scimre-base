1b8ae7ff0a9cc43d22c08a82aad255b05b2bd73d
masahino <masahino@gmail.com>
2017-03-29 22:30:14 +0900

    add eval_buffer

--- mrblib/eval.rb
+++ mrblib/eval.rb
@@ -11,5 +11,15 @@ module Mrbmacs
       @frame.view_win.sci_addtext(ret.to_s.length, ret.to_s)
       @frame.view_win.sci_newline
     end
+
+    def eval_buffer()
+      all_text = @frame.view_win.sci_get_text(@frame.view_win.sci_get_length+1)
+      begin
+        ret = eval(all_text)
+      rescue
+        $stderr.puts $!
+      end
+      $stderr.puts ret.to_s
+    end
   end
 end
