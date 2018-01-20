d3bb51f9750cb244cc58c2427149a13e6d3175f7
masahino <masahino@gmail.com>
2018-01-20 22:27:00 +0900

    add command args

--- mrblib/command.rb
+++ mrblib/command.rb
@@ -4,13 +4,20 @@ module Mrbmacs
       echo_win = @frame.echo_win
 #      command_list = @command_list.values.select {|item| item.kind_of?(String)}
       command_list = (Mrbmacs::Application.instance_methods - Object.instance_methods).map {|item| item.to_s}
-      command = @frame.echo_gets("M-x ") do |input_text|
+      input_str = @frame.echo_gets("M-x ") do |input_text|
         command_candidate = command_list.select {|item| item =~ /^#{input_text}/}
         [command_candidate.join(" "), input_text.length]
       end
-      if command != ""
+      if input_str != ""
+        args = input_str.split(/\s/)
+        command = args.shift
+        if args != []
+          args = "\"#{args.join(' ')}\""
+        else
+          args = nil
+        end
         begin
-          eval("#{command.gsub("-", "_")}()")
+          eval("#{command.gsub('-', '_')}(#{args})")
         rescue
           $stderr.puts $!
         end
