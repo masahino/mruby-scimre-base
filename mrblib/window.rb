13b1e250cb3bc3ad8ceeac78ec839f93a82cd0b6
masahino <masahino@gmail.com>
2018-01-20 22:24:17 +0900

    add window class

---
+++ mrblib/window.rb
@@ -0,0 +1,11 @@
+# coding: utf-8
+module Mrbmacs
+  class EditWindow
+    attr_accessor :sci, :frame
+    attr_accessor :buffer, :x1, :y1, :width, :height
+    def initialize(frame, buffer, x1, y1, width, height)
+      $stderr.puts "not yet implemented"
+    end
+
+  end
+end
