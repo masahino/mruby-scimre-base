52396f150d6aa77aaaa8c2bdfdaaa58c23224e9e
masahino <masahino@gmail.com>
2018-08-13 20:04:18 +0900

    enable to delete window

--- mrblib/window.rb
+++ mrblib/window.rb
@@ -45,17 +45,109 @@ module Mrbmacs
         @sci.sci_style_set_back(254, theme.font_color[:color_annotation][1])
         @sci.sci_annotation_set_visible(Scintilla::ANNOTATION_BOXED)
       end
+      @sci.sci_set_property("fold", "1")
+      @sci.sci_set_wrap_mode(Scintilla::SC_WRAP_CHAR)
     end
 
   end
 
   class Application
     def other_window
-      @frame.switch_window(@frame.edit_win_list.rotate!().first)
+      if @frame.edit_win_list.size > 1
+        index = @frame.edit_win_list.index(@frame.edit_win) + 1
+        if index >= @frame.edit_win_list.size
+          index = 0
+        end
+        @frame.switch_window(@frame.edit_win_list[index])
+#      @frame.switch_window(@frame.edit_win_list.rotate!().first)
+      end
     end
 
     def delete_window
-      $stderr.puts "delete window"
+      active_win = @frame.edit_win
+      if @frame.edit_win_list.size ==1
+        @frame.echo_puts("Atempt to delete sole ordinary window")
+        return
+      end
+      x1 = active_win.x1
+      x2 = active_win.x2
+      y1 = active_win.y1
+      y2 = active_win.y2
+      new_win = nil
+      count = 0
+      expand_wins = Hash.new()
+      expand_wins[:left] = []
+      expand_wins[:right] = []
+      expand_wins[:top] = []
+      expand_wins[:bottom] = []
+
+      @frame.edit_win_list.each do |e|
+        if e == active_win
+          next
+        end
+        new_win = e
+        ex1 = e.x1
+        ex2 = e.x2
+        ey1 = e.y1
+        ey2 = e.y2
+
+        if x1 == ex2 && y1 == ey1 && y2 == ey2
+          # left border
+          e.x2 = x2
+          count += 1
+        elsif x2 == ex1 && y1 == ey1 && y2 == ey2
+          # right border
+          e.x1 = x1
+          count += 1
+        elsif y1 == ey2 && x1 == ex1 && x2 == ex2
+          #  top border
+          e.y2 = y2
+          count += 1
+        elsif y2 == ey1 && x1 == ex1 && x2 == ex2
+          # bottom border
+          e.y1 = y1
+          count += 1
+        elsif x1 == ex2
+          expand_wins[:left].push e
+        elsif x2 == ex1
+          expand_wins[:right].push e
+        elsif y1 == ey2
+          expand_wins[:top].push e
+        elsif y2 == ey1
+          expand_wins[:bottom].push e
+        end
+      end
+      if count == 0
+        expand_wins.each_key do |d|
+          count = expand_wins[d].size
+          expand_wins[d].each do |w|
+            case d
+            when :left
+              w.x2 = x2
+            when :right
+              w.x1 =x1
+            when :top
+              w.y2 = y2
+            when :bottom
+              w.y1 = y1
+            end
+            w.compute_area
+          end
+          if count > 1
+            new_win = expand_wins[d].first
+            break
+          end
+        end
+      end
+      if count > 0
+        new_win.compute_area
+        @frame.switch_window(new_win)
+        active_win.delete
+        @frame.edit_win_list.delete(active_win)
+        @frame.edit_win_list.each do |w|
+          w.refresh
+        end
+      end
     end
 
     def delete_other_window
@@ -88,6 +180,7 @@ module Mrbmacs
       new_win = EditWindow.new(@frame, @current_buffer, x, y, width, height)
       @keymap.set_keymap(new_win.sci)
       new_win.set_default_style(@theme)
+      @current_buffer.mode.set_style(new_win.sci, @theme)
       @frame.edit_win_list.push(new_win)
       @frame.edit_win_list.each do |win|
         win.refresh()
