ff4663280216553e531c399380a909f2738a309d
masahino <masahino@gmail.com>
2019-04-04 22:08:49 +0900

    change the behavior of auto-completion

--- test/basic.rb
+++ test/basic.rb
@@ -76,7 +76,7 @@ assert('indent') do
   win = app.frame.view_win
   win.test_return[Scintilla::SCI_AUTOCACTIVE] = 1
   app.indent
-  assert_equal(Scintilla::SCI_TAB, app.frame.view_win.messages.pop)
+  assert_equal(Scintilla::SCI_VCHOME, app.frame.view_win.messages.pop)
   win.test_return[Scintilla::SCI_AUTOCACTIVE] = 0
   app.indent
   assert_equal(Scintilla::SCI_GETCOLUMN, app.frame.view_win.messages.pop)
