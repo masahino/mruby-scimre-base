8132f03b10fc315a0595696d35330cb50a3bed52
masahino <masahino@gmail.com>
2019-07-30 22:52:51 +0900

    delete libiconv on linux

--- .travis_build_config.rb
+++ .travis_build_config.rb
@@ -5,6 +5,11 @@ MRuby::Build.new do |conf|
   conf.gem :github => 'masahino/mruby-scintilla-base' do |g|
     g.download_scintilla
   end
+  conf.gem :github => 'mattn/mruby-iconv' do |g|
+    if RUBY_PLATFORM.include?('linux')
+      g.linker.libraries.delete 'iconv'
+    end
+  end
   conf.gem File.expand_path(File.dirname(__FILE__))
 
   conf.enable_test
