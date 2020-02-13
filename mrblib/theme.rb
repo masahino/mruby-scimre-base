d85bf31ee7d50aeb25a5af2ea813f2941ea61b0b
masahino <masahino@gmail.com>
2020-02-13 22:08:35 +0900

    set colors of default theme

--- mrblib/theme.rb
+++ mrblib/theme.rb
@@ -1,7 +1,6 @@
 module Mrbmacs
   class Application
     def select_theme(theme_name = nil)
-      $stderr.puts "select theme"
       if theme_name == nil
         theme_name = @frame.echo_gets("theme:",) do |input_text|
           comp_list = []
@@ -16,7 +15,6 @@ module Mrbmacs
           [comp_list.join(" "), input_text.length]
         end
       end
-      $stderr.puts theme_name
       if theme_name != nil
         @theme = @themes[theme_name].new
         if @theme.respond_to?(:set_pallete)
@@ -32,37 +30,37 @@ module Mrbmacs
     attr_accessor :style_list, :foreground_color, :background_color, :font_color, :name
     def initialize
       @name = "default"
-      @foregronud_color = 0xffffff
+      @foreground_color = 0xffffff
       @background_color = 0
       @style_list = {}
       @font_color = {
         # [fore, back, italic_flag, bold_flag ]
         :color_foreground => [@foreground_color, @background_color, nil, nil],
-        :color_builtin => [@foreground_color, @background_color, nil, nil],
-        :color_comment => [@foreground_color, @background_color, nil, nil],
-        :color_constant => [@foreground_color, @background_color, nil, nil],
-        :color_function_name => [@foreground_color, @background_color, nil, nil],
-        :color_keyword => [@foreground_color, @background_color, nil, nil],
-        :color_string => [@foreground_color, @background_color, nil, nil],
-        :color_type => [@foreground_color, @background_color, nil, nil],
-        :color_variable_name => [@foreground_color, @background_color, nil, nil],
-        :color_warning => [@foreground_color, @background_color, nil, nil],
-        :color_doc => [@foreground_color, @background_color, nil, nil],
-        :color_doc_string => [@foreground_color, @background_color, nil, nil],
-        :color_color_constant => [@foreground_color, @background_color, nil, nil],
-        :color_comment_delimiter => [@foreground_color, @background_color, nil, nil],
-        :color_preprocessor => [@foreground_color, @background_color, nil, nil],
-        :color_negation_char => [@foreground_color, @background_color, nil, nil],
-        :color_other_type => [@foreground_color, @background_color, nil, nil],
-        :color_regexp_grouping_construct => [@foreground_color, @background_color, nil, nil],
-        :color_special_keyword => [@foreground_color, @background_color, nil, nil],
-        :color_exit => [@foreground_color, @background_color, nil, nil],
-        :color_other_emphasized => [@foreground_color, @background_color, nil, nil],
-        :color_regexp_grouping_backslash => [@foreground_color, @background_color, nil, nil],
+        :color_builtin => [0x00ff00, @background_color, nil, nil],
+        :color_comment => [0xc0c0c0, @background_color, nil, nil],
+        :color_constant => [0x00ffff, @background_color, nil, nil],
+        :color_function_name => [0x0000ff, @background_color, nil, nil],
+        :color_keyword => [0x00ff00, @background_color, nil, nil],
+        :color_string => [0x00ffff, @background_color, nil, nil],
+        :color_type => [0xffff00, @background_color, nil, nil],
+        :color_variable_name => [0x0000ff, @background_color, nil, nil],
+        :color_warning => [0xff0000, @background_color, nil, nil],
+        :color_doc => [0xc0c0c0, @background_color, nil, nil],
+        :color_doc_string => [0xc0c0c0, @background_color, nil, nil],
+        :color_color_constant => [0x00ff00, @background_color, nil, nil],
+        :color_comment_delimiter => [0xc0c0c0, @background_color, nil, nil],
+        :color_preprocessor => [0x800000, @background_color, nil, nil],
+        :color_negation_char => [0xff0000, @background_color, nil, nil],
+        :color_other_type => [0x0000ff, @background_color, nil, nil],
+        :color_regexp_grouping_construct => [0x800000, @background_color, nil, nil],
+        :color_special_keyword => [0xff0000, @background_color, nil, nil],
+        :color_exit => [0xff0000, @background_color, nil, nil],
+        :color_other_emphasized => [0xff00ff, @background_color, nil, nil],
+        :color_regexp_grouping_backslash => [0xffff00, @background_color, nil, nil],
         # 
         :color_brace_highlight => [@background_color, @foreground_color, nil, nil],
-        :color_annotation => [@background_color, @foreground_color, true, nil],
-        :color_linenumber => [@foreground_color, @background_color, nil, nil],
+        :color_annotation => [@background_color, 0xff0000, true, nil],
+        :color_linenumber => [@foreground_color, 0x404040, nil, nil],
       }
     end
 
