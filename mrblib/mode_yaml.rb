c9f2d364556ce64850adfa27a7fa08dc7838b0bd
masahino <masahino@gmail.com>
2020-04-15 07:58:17 +0900

    add YamlMode

---
+++ mrblib/mode_yaml.rb
@@ -0,0 +1,29 @@
+module Mrbmacs
+  class YamlMode < Mode
+    include Scintilla
+    def initialize
+      super.initialize
+      @name = "yaml"
+      @lexer = "yaml"
+      @keyword_list = ""
+      @style = [
+        :color_foreground, #define SCE_YAML_DEFAULT 0
+        :color_comment, #define SCE_YAML_COMMENT 1
+        :color_string, #define SCE_YAML_IDENTIFIER 2
+        :color_keyword, #define SCE_YAML_KEYWORD 3
+        :color_foreground, #define SCE_YAML_NUMBER 4
+        :color_foreground, #define SCE_YAML_REFERENCE 5
+        :color_doc, #define SCE_YAML_DOCUMENT 6
+        :color_string, #define SCE_YAML_TEXT 7
+        :color_warning, #define SCE_YAML_ERROR 8
+        :color_foreground, #define SCE_YAML_OPERATOR 9
+          ]
+    end
+
+    def set_style(view_win, theme)
+      super
+      view_win.sci_set_property("fold.compact", "1")
+    end
+
+  end
+end
