1d62ef16f313047503b63b36b058f3779cb25184
masahino <masahino@gmail.com>
2024-08-13 06:51:16 +0900

    add tests

--- test/command.rb
+++ test/command.rb
@@ -1,10 +1,24 @@
-require File.dirname(__FILE__) + '/test_helper.rb'
+require "#{File.dirname(__FILE__)}/test_helper.rb"
 
 assert('change mode') do
   app = Mrbmacs::ApplicationTest.new
-  test_file = File.dirname(__FILE__) + "/test.input"
+  test_file = "#{File.dirname(__FILE__)}/test.input"
   app.find_file(test_file)
-  assert_equal "fundamental", app.current_buffer.mode.name
+  assert_equal 'fundamental', app.current_buffer.mode.name
   app.ruby_mode
-  assert_equal "ruby", app.current_buffer.mode.name
+  assert_equal 'ruby', app.current_buffer.mode.name
+end
+
+assert('respond_to_missing?') do
+  app = Mrbmacs::ApplicationTest.new
+
+  # Test responding to existing methods
+  assert_true app.respond_to?(:SCI_PRESS)
+  assert_true app.respond_to?(:SCI_RELEASE)
+  assert_true app.respond_to?(:sci_foobar)
+  assert_true app.respond_to?(:ruby_mode)
+
+  # Test non-existent methods
+  assert_false app.respond_to?(:non_existent_method)
+  assert_false app.respond_to?(:random_method)
 end
